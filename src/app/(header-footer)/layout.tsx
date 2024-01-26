4fb03066b164cca1f3bc853fa26b24b6f087e750
Vaibhav Jaiswar <vaibhavjaiswar98@gmail.com>
2024-01-26 22:35:01 +0530

    Removing GTM components from DOM

--- src/app/(header-footer)/layout.tsx
+++ src/app/(header-footer)/layout.tsx
@@ -1,11 +1,11 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-import { Suspense } from 'react'
+// import { Suspense } from 'react'
 import '@/app/globals.css'
 import Header from "@/components/header"
 import Footer from "@/components/footer"
 import SuperKartProvider from '@/redux/provider'
-import GTMAnalytics from '@/components/GTMComponent'
+// import GTMAnalytics from '@/components/GTMComponent'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -18,9 +18,9 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <body className={`${inter.className} mx-auto max-w-[1556px] bg-neutral-50`}>
-        <Suspense>
+        {/* <Suspense>
           <GTMAnalytics />
-        </Suspense>
+        </Suspense> */}
         <SuperKartProvider>
           <Header />
           <main className="min-h-[75vh]">
